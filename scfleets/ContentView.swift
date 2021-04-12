a21769dfd7b5af89e0be9ce611ce5d16def0b002
Kyle Walter <walter.kl26@gmail.com>
2021-04-12 17:55:23 -0500

    Initial Commit

---
+++ scfleets/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  scfleets
+//
+//  Created by Kyle Walter on 4/12/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ scfleets/scfleetsApp.swift
@@ -0,0 +1,17 @@
+//
+//  scfleetsApp.swift
+//  scfleets
+//
+//  Created by Kyle Walter on 4/12/21.
+//
+
+import SwiftUI
+
+@main
+struct scfleetsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
